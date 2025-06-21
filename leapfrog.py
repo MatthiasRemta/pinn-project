0514c7581a918b5249c0f49c7c5735d618a99953
mremta <matthias.remta@cern.ch>
2025-06-21 23:02:49 +0200

    initial commit

---
+++ leapfrog.py
@@ -0,0 +1,23 @@
+import torch
+
+class Hamiltonian():
+    def __init__(self, hamiltonian_fn, q_prime, p_prime):
+        self.H = hamiltonian_fn
+        self.q_prime = q_prime
+        self.p_prime = p_prime
+    def q_prime(self, q, p, s):
+        return self.q_prime(q, p, s)
+
+    def p_prime(self, q, p):
+        return self.p_prime(q, p, s)
+
+class Leapfrog():
+    def __init__(self, hamiltonian, step_size):
+        self.hamiltonian = hamiltonian
+        self.step_size = step_size
+
+    def step(self, q, p, s):
+        p_half = p + 0.5 * self.step_size * self.hamiltonian.p_prime(q, p, s)
+        q_new = q + self.step_size * self.hamiltonian.q_prime(q, p_half, s)
+        p_new = p_half + 0.5 * self.step_size * self.hamiltonian.p_prime(q_new, p_half, s)
+        return q_new, p_new
